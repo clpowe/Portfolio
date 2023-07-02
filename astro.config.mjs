0db4d55ff8ef4c1ebad75d19175b7da4a100a0ef
clpowe <clpowe@gmail.com>
2023-07-02 00:31:47 -0400

    initialSetup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,27 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import UnoCSS from 'unocss/astro'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		plugins: [basicSsl()],
+		server: {
+			https: true
+		}
+	},
+	integrations: [
+		storyblok({
+			accessToken: env.STORYBLOK_TOKEN,
+			apiOptions: {
+				region: 'us'
+			}
+		}),
+		UnoCSS({
+			injectReset: true
+		})
+	]
+})
